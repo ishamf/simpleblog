ca8a30604bff689280335c6d95aeab01dd762fc9
rmxhaha <code000123@gmail.com>
2015-05-12 20:53:29 +0700

    memperjelas form

--- add.php
+++ add.php
@@ -28,18 +28,22 @@ if( isset( $_POST['post-title'] ) && isset( $_POST['post-content']) ){
 	<form action='add.php' method=POST class=addpostform>
 	<table>
 		<tr>
-			<td>
+			<td colspan=2>
 				<h1>Add Post</h1>
 		<tr>
+			<td>Judul : 
 			<td>
 				<input name='post-title' style="width:640px; font-family : Arial" autofocus />
 		<tr>
+			<td> Tanggal :
 			<td>
 				<input name='post-date' id=datepicker type=text />
 		<tr>
+			<td valign=top> Konten : 
 			<td>
 				<textarea name='post-content' style="width:640px; height : 300px; font-family : Arial"></textarea>
 		<tr>
+			<td>
 			<td>
 				<input type=submit name='submit' value='add' />
 	</table>
