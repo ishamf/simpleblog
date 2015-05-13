110f7edaa21e54d313aa92af9719614cefd608b2
Isham Faizal <ishamf@users.noreply.github.com>
2015-05-13 14:35:44 +0700

    Moved editor code to editor.php

--- admin/add.php
+++ admin/add.php
@@ -9,91 +9,11 @@ if( isset( $_POST['post-title'] ) && isset( $_POST['post-content']) ){
 	$date = $_POST['post-date'];
 	
 	$blog->addPost( $title, $content, $date );
+} else {
+	$post_content = ''; $post_title = ''; $post_date = '';
+
+	include('editor.php';)
 }
 
 ?>
-
-<!doctype HTML>
-<html lang=en>
-<head>
-	<title>SimpleBlog - Add Post </title>
-	<link rel="stylesheet" href="../css/smoothness/jquery-ui.css">
-	<script src="../js/jquery-1.10.2.js"></script>
-	<script src="../js/jquery-ui.js"></script>
-</head>
-
-<body>
-	<form action='add.php' method=POST class=addpostform>
-	<table>
-		<tr>
-			<td colspan=2>
-				<h1>Add Post</h1>
-		<tr>
-			<td>Judul : 
-			<td>
-				<input name='post-title' style="width:640px; font-family : Arial" autofocus />
-		<tr>
-			<td> Tanggal :
-			<td>
-				<input name='post-date' id=datepicker type=text />
-				<span id=post-date-error></span>
-		<tr>
-			<td valign=top> Konten : 
-			<td>
-				<textarea name='post-content' style="width:640px; height : 300px; font-family : Arial"></textarea>
-		<tr>
-			<td>
-			<td>
-				<input type=submit name='submit' value='add' />
-	</table>
-
-	<script>
-	$(function() {
-		$( "#datepicker" ).datepicker({
-			minDate : new Date(),
-			defaultDate : new Date()
-			
-		});
-	
-		function isValidPostDate(){
-			var d = new Date();
-			var strippedD = new Date(d.getFullYear(), d.getMonth(), d.getDay() );
-			// pake Date.parse karena kalau new Date bisa ngaco kalau tahun nya < 1970
-			var selectedTime = Date.parse( $('#datepicker').val() );
-			
-			return selectedTime >= strippedD.getTime();
-		}
-	
-		function validateDate(){
-			if( !isValidPostDate() )
-			{
-				$('#post-date-error')
-					.html('Error : Tanggal seharusnya lebih dari hari ini')
-					.show();
-				return false;
-			}
-			else {
-				$('#post-date-error')
-					.hide();
-			}
-			return true;
-		}
-	
-		$('#datepicker').change(function( event ){
-			validateDate();
-		});
-	
-		$('.addpostform').submit(function( event ){
-			if( !validateDate() )
-			{
-				event.preventDefault();
-			}
-		});
-	});
-	
-	</script>
-
-	</form>
-</body>
-</html>
 
--- admin/edit.php
+++ admin/edit.php
@@ -29,88 +29,9 @@ else {
 	$post_content = $data['content'];
 	$post_title = $data['title'];
 	$post_date = $data['date_created'];
+
+	include('editor.php');
 }
 
 
 ?>
-
-<html>
-<head>
-	<title>Edit Post</title>
-	<link rel="stylesheet" href="../css/smoothness/jquery-ui.css">
-	<script src="../js/jquery-1.10.2.js"></script>
-	<script src="../js/jquery-ui.js"></script>
-</head>
-<body>
-	<form action="edit.php?id=<?php echo $post_id; ?>" method=POST>
-	<table>
-		<tr>
-			<td colspan=2>
-				<h1>Edit Post</h1>
-		<tr>
-			<td>Judul : 
-			<td>
-				<input name='post-title' style="width:640px; font-family : Arial" value="<?php echo $post_title; ?>" />
-		<tr>
-			<td> Tanggal :
-			<td>
-				<input name='post-date' id=datepicker type=text value="<?php echo $post_date ?>" />
-				<span id=post-date-error></span>
-		<tr>
-			<td valign=top> Konten : 
-			<td>
-				<textarea name='post-content' style="width:640px; height : 300px; font-family : Arial" autofocus><?php echo $post_content ?></textarea>
-		<tr>
-			<td>
-			<td>
-				<input type=submit name='submit' value='change' />
-	</table>
-	<script>
-	$(function() {
-		$( "#datepicker" ).datepicker({
-			minDate : new Date(),
-			defaultDate : new Date()
-			
-		});
-	
-		function isValidPostDate(){
-			var d = new Date();
-			var strippedD = new Date(d.getFullYear(), d.getMonth(), d.getDay() );
-			// pake Date.parse karena kalau new Date bisa ngaco kalau tahun nya < 1970
-			var selectedTime = Date.parse( $('#datepicker').val() );
-			
-			return selectedTime >= strippedD.getTime();
-		}
-	
-		function validateDate(){
-			if( !isValidPostDate() )
-			{
-				$('#post-date-error')
-					.html('Error : Tanggal seharusnya lebih dari hari ini')
-					.show();
-				return false;
-			}
-			else {
-				$('#post-date-error')
-					.hide();
-			}
-			return true;
-		}
-	
-		$('#datepicker').change(function( event ){
-			validateDate();
-		});
-	
-		$('.addpostform').submit(function( event ){
-			if( !validateDate() )
-			{
-				event.preventDefault();
-			}
-		});
-	});
-	
-	</script>
-	
-	</form>
-</body>
-</html>
