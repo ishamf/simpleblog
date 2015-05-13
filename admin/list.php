e9b6baac5da4c8c714b24f3fa7ba63fefed4614e
rmxhaha <code000123@gmail.com>
2015-05-13 17:39:13 +0700

    adding delete button

--- admin/list.php
+++ admin/list.php
@@ -10,6 +10,7 @@ while( $row = $resource->fetch_assoc() ){
 		<h2>$row[title]</h2>
 		$row[date_created]
 		<a href=\"edit.php?id=$row[id]\">Edit</a>
+		<a href=\"delete.php?id=$row[id]\" onclick=\"return confirm('Are you Sure ?');\">Delete</a>
 		<p>
 		$row[content]
 		</p>
