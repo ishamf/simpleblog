8f8b6895ce28c571ba307e61e029d90b4074d253
rmxhaha <code000123@gmail.com>
2015-05-13 08:41:15 +0700

    remove debug code

--- lib/SimpleBlog.php
+++ lib/SimpleBlog.php
@@ -45,9 +45,6 @@ class SimpleBlog {
 			VALUES ('$title','$content','$date');
 		";
 		
-		echo $sql;
-		
-		
 		if( !$result = $db->query( $sql )) {
 			die('There was an error running the query [' . $db->error . ']');
 		}
